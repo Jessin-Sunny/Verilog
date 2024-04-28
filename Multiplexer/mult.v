4392bdf963fbd3975c220fe5c760660ff384cea9
Jessin Sunny <119120870+Jessin-Sunny@users.noreply.github.com>
2024-04-28 13:08:07 +0530

    Update mult.v

--- Multiplexer/mult.v
+++ Multiplexer/mult.v
@@ -1,7 +1,7 @@
 module mult(
-  input i0,1,i2,i3,s0,s1,
+  input i0,i1,i2,i3,s0,s1,
   output y);
   
-  assign y=(i0&((~s0)&(~s1)))|(i1&(s0&(~s1)))|(i2&((~s0)&s1))|(i3&(s1&s2));
+  assign y=(i0&((~s0)&(~s1)))|(i1&(s0&(~s1)))|(i2&((~s0)&s1))|(i3&(s0&s1));
   
 endmodule
